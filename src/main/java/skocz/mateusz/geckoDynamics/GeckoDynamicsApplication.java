b25233beabfa8fb7813d4bc3010436b3b22ef6e9
matt <matt@MacBook-Pro.local>
2020-06-24 18:28:25 +0200

    Initial commit

---
+++ src/main/java/skocz/mateusz/geckoDynamics/GeckoDynamicsApplication.java
@@ -0,0 +1,13 @@
+package skocz.mateusz.geckoDynamics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeckoDynamicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeckoDynamicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skocz/mateusz/geckoDynamics/GeckoDynamicsApplicationTests.java
@@ -0,0 +1,13 @@
+package skocz.mateusz.geckoDynamics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeckoDynamicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
